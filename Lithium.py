c129809f45d740f023460e728517d7c3aa1387f0
alex walters <tritium@sdamon.com>
2017-02-22 06:15:06 -0500

    Initial Commit

---
+++ Lithium.py
@@ -0,0 +1,34 @@
+import asyncio
+import logging
+import os
+
+import diceparse
+import discord
+
+logging.basicConfig(level=logging.INFO)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    logging.info(
+        "Logged in as %s, %s",
+        client.user.name,
+        client.user.id
+    )
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!r"):
+        _, _, rest = message.content.partition(" ")
+        nick = message.author.display_name
+        lines = diceparse.roll(rest, who=nick).split("\n")
+        for line in lines:
+            await client.send_message(message.channel, line)
+
+def main():
+    token = os.getenv('LITHIUM_TOKEN', input("Provide API Token").strip())
+    client.run(token)
+
+if __name__ == "__main__":
+    main()
